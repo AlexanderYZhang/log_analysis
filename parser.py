62fe57ed54b7b766dc20a8ba9f5c456bb4658db5
AlexanderYZhang <alexzhangn@gmail.com>
2017-02-22 18:51:17 -0500

    added byte infor

--- parser.py
+++ parser.py
@@ -6,11 +6,11 @@ fs = open("files/NASA_access_log_Aug95")
 fo = open("results.txt","w" )
 
 res = ""
-fo.write("remote_host\trequest_method\trequest_url\tstatus\ttime_received\n")
+fo.write("remote_host\trequest_method\trequest_url\tstatus\ttime_received\tbytes\n")
 
 for line in fs:
 	 json = line_parser(line)
 	 time = json['time_received_utc_isoformat'].split('T')[0].split('-')[2]
-	 res = json['remote_host'] + "\t" + json['request_method'] + "\t" + json['request_url'] + "\t" + json['status'] + "\t" + time + "\n"
+	 res = json['remote_host'] + "\t" + json['request_method'] + "\t" + json['request_url'] + "\t" + json['status'] + "\t" + time + "\t" + json['response_bytes_clf'] + "\n"
 	 fo.write(res)
 
