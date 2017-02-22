a3bd9c9118732afe9d3c9179d2c68391693e6b17
AlexanderYZhang <alexzhangn@gmail.com>
2017-02-22 15:06:57 -0500

    added header

--- parser.py
+++ parser.py
@@ -2,10 +2,11 @@ import apache_log_parser
 from pprint import pprint
 
 line_parser = apache_log_parser.make_parser("%h %l %u %t \"%r\" %>s %b")
-fs = open("NASA_access_log_Aug95")
+fs = open("files/NASA_access_log_Aug95")
 fo = open("results.txt","w" )
 
 res = ""
+fo.write("remote_host,request_method,request_url,status,time_received\n")
 
 for line in fs:
 	 json = line_parser(line)
